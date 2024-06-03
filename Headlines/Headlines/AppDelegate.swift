bcd7892235df0eb60fa4cc3a54a70dd1b07de031
riachandra25 <37836843+riachandra25@users.noreply.github.com>
2024-06-03 09:51:41 +0100

    Update AppDelegate.swift

--- Headlines/Headlines/AppDelegate.swift
+++ Headlines/Headlines/AppDelegate.swift
@@ -8,7 +8,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    var mainCoordinator: MainCoordinator?
+    var mainCoordinator: Coordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
